ff8b1ccbed98c29ea42f5926bb16934c3d76ab76
Aefvvges <torri.barbara4@gmail.com>
2024-11-09 18:50:43 -0300

    tercer commit

---
+++ loadHeader.js
@@ -0,0 +1,9 @@
+fetch('header.html')
+.then(response => {
+  if (!response.ok) throw new Error('Error al cargar el encabezado');
+  return response.text();
+})
+.then(data => {
+  document.getElementById('header-container').innerHTML = data;
+})
+.catch(error => console.error(error));
